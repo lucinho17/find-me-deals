4a577980e00cf78180ceb1ee57b45850511098ec
Luka Borović <124844612+lucinho17@users.noreply.github.com>
2025-12-23 21:32:03 +0100

    fix: correct base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/find-me-deals/',
+  base: '/find-me-some-deals/',
   plugins: [react()],
 })
